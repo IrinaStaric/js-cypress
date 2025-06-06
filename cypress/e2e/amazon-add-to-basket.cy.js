fe6bc06d21cab358a3d01b675cb4666faf489437
irina.staric1 <irina.staric@gmail.com>
2025-06-06 20:27:55 +0100

    Fix add to basket test and simplify logic

--- cypress/e2e/amazon-add-to-basket.cy.js
+++ cypress/e2e/amazon-add-to-basket.cy.js
@@ -1,20 +1,15 @@
-describe('Amazon product – add to basket flow', () => {
-    it('declines cookies, selects option, and adds item to basket', () => {
+describe('Amazon product - add to basket flow', () => {
+    it('declines cookies and adds item to basket', () => {
       cy.visit('https://www.amazon.co.uk/KeySmart-Compatible-Keychain-Keyholder-Organizer/dp/B0BNLV872G');
   
-      // 1. Decline cookies
+      // Decline cookies
       cy.get('#sp-cc-rejectall-link', { timeout: 10000 }).click({ force: true });
   
-    
-      cy.get('li[class*="swatchAvailable"]', { timeout: 10000 }).first().click({ force: true });
-      cy.get('input#add-to-cart-button', { timeout: 15000 })
-        .scrollIntoView()
-        .should('be.visible')
-        .click();
-  
-      cy.contains('Added to Basket', { timeout: 10000 }).should('exist');
+      cy.get('#add-to-cart-button', { timeout: 10000 }).click();
+      cy.url().should('include', '/cart');
     });
   });
+  
   
   
   
