1dc70f93c3bf8f3670262dcf34ebc2d3c0ef6f1f
irina.staric1 <irina.staric@gmail.com>
2025-06-06 19:56:57 +0100

    Fix: updated basket test for stability

--- cypress/e2e/amazon-add-to-basket.cy.js
+++ cypress/e2e/amazon-add-to-basket.cy.js
@@ -1,20 +1,21 @@
 describe('Amazon product – add to basket flow', () => {
-    it('declines cookies and adds item to basket', () => {
+    it('declines cookies, selects option, and adds item to basket', () => {
       cy.visit('https://www.amazon.co.uk/KeySmart-Compatible-Keychain-Keyholder-Organizer/dp/B0BNLV872G');
   
-      cy.wait(4000);
-  
-      // Decline cookies
+      // 1. Decline cookies
       cy.get('#sp-cc-rejectall-link', { timeout: 10000 }).click({ force: true });
   
-      cy.get('input[name="submit.add-to-cart"]', { timeout: 15000 })
+    
+      cy.get('li[class*="swatchAvailable"]', { timeout: 10000 }).first().click({ force: true });
+      cy.get('input#add-to-cart-button', { timeout: 15000 })
         .scrollIntoView()
         .should('be.visible')
         .click();
   
-      cy.contains('Added to basket', { timeout: 10000 }).should('exist');
+      cy.contains('Added to Basket', { timeout: 10000 }).should('exist');
     });
   });
+  
   
   
   
