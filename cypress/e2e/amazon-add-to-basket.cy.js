07c38e4b86b43a22e6fbfb4ebece70f6adfa4294
irina.staric1 <irina.staric@gmail.com>
2025-06-06 19:49:39 +0100

    Fix: more stable selector for add-to-cart button

--- cypress/e2e/amazon-add-to-basket.cy.js
+++ cypress/e2e/amazon-add-to-basket.cy.js
@@ -1,13 +1,21 @@
 describe('Amazon product – add to basket flow', () => {
     it('declines cookies and adds item to basket', () => {
-        
       cy.visit('https://www.amazon.co.uk/KeySmart-Compatible-Keychain-Keyholder-Organizer/dp/B0BNLV872G');
   
+      cy.wait(4000);
+  
+      // Decline cookies
       cy.get('#sp-cc-rejectall-link', { timeout: 10000 }).click({ force: true });
-      cy.get('#add-to-cart-button', { timeout: 15000 }).should('be.visible').click();
-      cy.contains('Added to basket').should('exist');
+  
+      cy.get('input[name="submit.add-to-cart"]', { timeout: 15000 })
+        .scrollIntoView()
+        .should('be.visible')
+        .click();
+  
+      cy.contains('Added to basket', { timeout: 10000 }).should('exist');
     });
   });
+  
   
   
   
