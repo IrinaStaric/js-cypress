ce7e83d9b413e91cc39d40ad5b55c36a0d12805f
irina.staric1 <irina.staric@gmail.com>
2025-06-07 14:04:14 +0100

    Remove unused files

--- task4-palindrome.js
+++
@@ -1,9 +0,0 @@
-function isPalindrome(str) {
-    const cleaned = str.toLowerCase().replace(/[\W_]/g, '');
-    return cleaned === cleaned.split('').reverse().join('');
-  }
-  
-  console.log(isPalindrome("Radar"));        // true
-  console.log(isPalindrome("Not a Palindrome")); // false
-  
-  
